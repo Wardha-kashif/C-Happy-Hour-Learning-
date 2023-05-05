a0d2744aa1739946a92b2019830f9dde0cebff75
warda.kashif <warda.kashif@logicielservice.com>
2023-05-05 15:51:57 +0500

    Problem Solving

---
+++ ProblemSolving(.net)/ProblemSolving(.net)/Egypt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving_.net_
+{
+    public class Egypt
+    {
+
+        public void EgyptFunc()
+        {
+          
+                Console.WriteLine("Enter Sides of Traingle");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int num3 = Convert.ToInt32(Console.ReadLine());
+
+                    if (num1 > 0 && num2 >  0 && num3 > 0)
+                    {
+                        if (num1 * num1 == num2 * num2 + num3 * num3)
+                        {
+                            Console.WriteLine("right");
+                        }
+                        if (num2 * num2 == num1 * num1 + num3 * num3)
+                        {
+                            Console.WriteLine("right");
+                        }
+
+                        if (num3 * num3 == num1 * num1 + num2 * num2)
+                        {
+                            Console.WriteLine("right");
+                        }
+                else
+                {
+                    Console.WriteLine("wrong");
+
+                }
+                    }
+                    else
+                    {
+                        Console.WriteLine("wrong");
+               
+                    }
+            }
+        }
+    }
+
---
+++ ProblemSolving(.net)/ProblemSolving(.net)/program.cs
@@ -0,0 +1,11 @@
+﻿using ProblemSolving_.net_;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Egypt obj1=new Egypt();
+        obj1.EgyptFunc();
+    }
+}
